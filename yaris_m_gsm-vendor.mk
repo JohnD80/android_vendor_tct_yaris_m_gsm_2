a6ba03544787cc1754d944ea994fc7d38c50f470
kirito96 <eagle1968@live.com>
2016-12-05 20:54:24 -0400

    removed unused line

--- yaris_m_gsm-vendor.mk
+++ yaris_m_gsm-vendor.mk
@@ -4,8 +4,6 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/bin
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/vendor,system/vendor)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/xbin,system/xbin)
 
 
