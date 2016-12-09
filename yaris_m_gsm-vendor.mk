a93b44e2892928b72d3c0214b4b5cf4c471fc5bc
kirito96 <eagle1968@live.com>
2016-12-08 20:40:46 -0400

    Switch to Yaris_M_GSM

---
+++ yaris_m_gsm-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/xbin,system/xbin)
+
